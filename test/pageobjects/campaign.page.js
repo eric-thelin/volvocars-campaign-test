78ab37e918957644f9e6c2bccbc5820612dc4c72
Eric Thelin <eric.thelin@zingtongroup.com>
2023-01-13 15:58:26 +0100

    Add minimal test for campaign page

---
+++ test/pageobjects/campaign.page.js
@@ -0,0 +1,13 @@
+
+
+class CampaignPage {
+	open () {
+		return browser.url('intl/v/car-safety/a-million-more')
+	}
+
+    get heading () {
+        return $('h2');
+    }
+}
+
+export default new CampaignPage();
