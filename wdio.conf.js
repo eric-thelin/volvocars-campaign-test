0d69702cf35e0d7d1443edcf8c3220a8228ca382
Eric Thelin <eric.thelin@zingtongroup.com>
2023-01-16 15:57:57 +0100

    Remove acceptance of insecure certificates

--- wdio.conf.js
+++ wdio.conf.js
@@ -66,15 +66,12 @@ export const config = {
 		maxInstances: 1,
 		//
 		browserName: 'chrome',
-		acceptInsecureCerts: true
 		// If outputDir is provided WebdriverIO can capture driver session logs
 		// it is possible to configure which logTypes to include/exclude.
 		// excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
 		// excludeDriverLogs: ['bugreport', 'server'],
 	}, {
-
 		browserName: 'firefox',
-		acceptInsecureCerts: true
 	}],
 	//
 	// ===================
