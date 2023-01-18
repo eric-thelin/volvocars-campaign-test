36cb072c3eaa27ea8165511d86b7381c81198d75
Eric Thelin <eric.thelin@zingtongroup.com>
2023-01-18 11:08:05 +0100

    Add visual regression test
    See: https://webdriver.io/docs/wdio-image-comparison-service

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
 FROM node:19.4.0-bullseye
+RUN apt-get update \
+		&& apt-get install --yes libcairo2-dev libjpeg62-turbo-dev libpango1.0-dev libgif-dev build-essential g++ \
+		&& rm -rf /var/lib/apt/lists/*
 WORKDIR app
 ADD --chown=node:node package.json package-lock.json ./
 RUN npm install
--- test/specs/campaign.e2e.js
+++ test/specs/campaign.e2e.js
@@ -3,7 +3,6 @@ import CampaignPage from '../pageobjects/campaign.page.js'
 describe('Campaign', () => {
 	it('should have expected content', async () => {
 		await CampaignPage.open();
-
 		expect(await CampaignPage.getHeadings()).toEqual([
 			'Ideas that change the world are often the most controversial.',
 			'One of a million',
@@ -11,5 +10,9 @@ describe('Campaign', () => {
 			'Explore our models',
 			'',
 		]);
+
+		let imageComparisonOptions = {'hideElements': await $$('video[autoplay], button[aria-label=pause]')};
+		expect(await browser.checkFullPageScreen('campaign-page', imageComparisonOptions)).toEqual(0);
+		expect(await browser.checkTabbablePage('campaign-page-tab-order', imageComparisonOptions)).toEqual(0);
 	})
 })
